b769f019654de7946007109865e0074325d09495
heejuu <heejuu321@gmail.com>
2022-12-13 09:46:01 +0900

    hj

--- Buddy/src/main/java/com/chat/controller/HomeController.java
+++ Buddy/src/main/java/com/chat/controller/HomeController.java
@@ -1,54 +1,54 @@
-package com.chat.controller;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model, HttpSession session) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);		
-		session.setAttribute("id", "heejoo");
-
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "login";
-	}
-	
-	@RequestMapping(value = "/homePost", method = RequestMethod.POST)
-	public String homePost(Model model, Locale locale, HttpSession session, String id) {
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);		
-		session.setAttribute("id", id);
-		logger.info("받은 id : {}.", id);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
-}
+//package com.chat.controller;
+//
+//import java.text.DateFormat;
+//import java.util.Date;
+//import java.util.Locale;
+//import java.util.Map;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@Controller
+//public class HomeController {
+//	
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model, HttpSession session) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);		
+//		session.setAttribute("id", "heejoo");
+//
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "login";
+//	}
+//	
+//	@RequestMapping(value = "/homePost", method = RequestMethod.POST)
+//	public String homePost(Model model, Locale locale, HttpSession session, String id) {
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);		
+//		session.setAttribute("id", id);
+//		logger.info("받은 id : {}.", id);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "home";
+//	}
+//	
+//}
